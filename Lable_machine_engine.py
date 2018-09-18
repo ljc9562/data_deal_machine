34241a09159cc9d3a013a7bdde8fb33198d36e72
ljc <854426089@qq.com>
2018-09-19 03:13:31 +0800

    wow

---
+++ Lable_machine_engine.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_hdf(r"Z:\automation\new_tot_table\output\tot_data.h5")
+
---
+++ welookup_machine.py
@@ -0,0 +1,55 @@
+###
+#是一个多数据源匹配整合工具
+###
+import pandas as pd
+from os import getcwd,listdir
+import sys
+
+class Input_Data:
+    def __init__(self,aim_soure,match_soure):
+        self.aim = aim_soure
+        self.match = match_soure
+
+    def whats_this(self):
+
+
+    def read_file(self):
+
+
+class Both_collect:
+    def __init__(self):
+
+    def one_on_one(self):
+
+
+    def one_on_more(self):
+
+
+    def more_on_more(self):
+
+
+class Output:
+    def __init__(self):
+
+
+    def sql_output(self):
+
+
+    def file_output(self):
+
+
+class welookup:
+    def __init__(self,aim_soure,match_soure,aim_columns_name,match_columns_name,match_info):
+        self.aim_soure, self.match_soure, self.aim_columns_name, self.match_columns_name, self.match_info = aim_soure,match_soure,aim_columns_name,match_columns_name,match_info
+
+    def judge
+
+    def summary(self):
+        aim_soure,match_soure = Input_Data.read_file()
+
+
+
+
+
+if __name__ == '__main__':
+    run = welookup(aim_dir=sys.argv[1],match_dir=sys.argv,aim_columns_name=sys.argv,match_columns_name=sys.argv,match_info=sys.argv)
