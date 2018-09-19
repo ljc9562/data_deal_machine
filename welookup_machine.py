dce8ef712d2f1acc0721abf9e8ab8f533e2eefd7
ljc9562 <854426089@qq.com>
2018-09-19 18:14:29 +0800

    2018年9月19日 12:56:40

--- welookup_machine.py
+++ welookup_machine.py
@@ -11,7 +11,7 @@ import pandas as pd
 import sys
 
 class Both_collect:
-    def __init__(self,aim_columns_name,match_columns_name,needed_columns,datasoure_left,datasoure_right):
+    def __init__(self,aim_columns_name,match_columns_name,needed_columns,datasoure_left,datasoure_right,lable,combine = True):
         self.aim_columns_name = aim_columns_name.split("$")
         self.match_columns_name = match_columns_name.split("$")
         self.needed_columns = needed_columns.split("$")
@@ -20,14 +20,17 @@ class Both_collect:
         _need_columns = self.match_columns_name
         _need_columns += [value for value in self.needed_columns]
         self.Data_orginal_right = self.Data_orginal_right.loc[:,_need_columns]
+        self.combine = combine
+        self.lable = lable
+
 
     def matched_key_deal(self):
         _needed_columns_deal = self.needed_columns.copy()
         _needed_columns_deal.append(self.match_columns_name[0])
-        self.Data_orginal_right_deal = self.Data_orginal_right.loc[:,_needed_columns_deal]
+        Data_orginal_right_deal = self.Data_orginal_right.loc[:,_needed_columns_deal]
         _needed_columns_deal.pop()
         _needed_columns_deal.append('key')
-        self.Data_orginal_right_deal.columns = _needed_columns_deal
+        Data_orginal_right_deal.columns = _needed_columns_deal
         _needed_columns_deal.pop()
 
         if len(self.match_columns_name)>1:
@@ -38,21 +41,38 @@ class Both_collect:
                 _needed_columns_deal.append('key')
                 Data_orginal_right_deal_more.columns = _needed_columns_deal
                 _needed_columns_deal.pop()
-                self.Data_orginal_right_deal = pd.concat([self.Data_orginal_right_deal,Data_orginal_right_deal_more],axis=0,ignore_index=True)
-        return self.Data_orginal_right_deal
+                Data_orginal_right_deal = pd.concat([Data_orginal_right_deal,Data_orginal_right_deal_more],axis=0,ignore_index=True)
+        return Data_orginal_right_deal
+
+
+    def combine(self):
+
+
 
     def aim_match(self):
-        welookup_data = pd.merge(left=self.Data_orginal_left, right=self.Data_orginal_right_deal, how='left',left_on=self.aim_columns_name[0], right_on='key')
+        _Data_orginal_right_deal = self.matched_key_deal()
+        _needed_columns_deal2 = self.needed_columns.copy()
+        _needed_columns_deal2.append(self.match_columns_name[0])
+        welookup_data = pd.merge(left=self.Data_orginal_left, right=_Data_orginal_right_deal, how='left',left_on=self.aim_columns_name[0], right_on='key')
         for aim_col_num in range(1,len(self.aim_columns_name)):
-            welookup_data = pd.merge(left = welookup_data , right= self.Data_orginal_right_deal,how='left',left_on=self.aim_columns_name[aim_col_num],right_on = 'key')
+            welookup_data = pd.merge(left = welookup_data , right= _Data_orginal_right_deal,how='left',left_on=self.aim_columns_name[aim_col_num],right_on = 'key')
+        welookup_data = welookup_data.fillna("")
+        _combine_columns = welookup_data.columns.tolist()[len(welookup_data.columns.tolist())-len(self.aim_columns_name)*(len(self.needed_columns)+1):]
+        if  self.combine:
+            for columns in _needed_columns_deal2:
+                welookup_data[f'{columns}_{self.lable}'] = ""
+
+                return welookup_data
+        else:
+            return welookup_data
 
 
-    def judge_match_relationship(self):
-        if len(self.match_columns_name) > 1:
+
+
 
 
 class welookup:
-    def __init__(self, aim_soure, match_soure, aim_columns_name, match_columns_name, needed_columns):
+    def __init__(self, aim_soure, match_soure, aim_columns_name, match_columns_name, needed_columns,lable,combine = True):
         '''
         -----------------------------------------------------------------------------------------------------------------
         :param aim_soure: 目标数据源（匹配合并数据）
@@ -60,9 +80,12 @@ class welookup:
         :param aim_columns_name: 用于左连接的key列名
         :param match_columns_name: 用于被连接的key列名
         :param needed_columns: 需要被匹配到aim_soure中的列名
+        :param lable：输出匹配列的备注标签 例如：备注标签：现金券  匹配的结果：店铺id_现金券
+        :param combine：是否合并多对多匹配出来的多个列
         -----------------------------------------------------------------------------------------------------------------
         '''
         self.aim_soure, self.match_soure, self.aim_columns_name, self.match_columns_name, self.needed_columns = aim_soure, match_soure, aim_columns_name, match_columns_name, needed_columns
+        self.lable,self.combine = lable,combine
 
     def summary(self):
         '''
