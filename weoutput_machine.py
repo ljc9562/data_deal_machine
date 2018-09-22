c3699708c85f0134797da0b6f990d098710cb308
ljc9562 <854426089@qq.com>
2018-09-23 02:09:52 +0800

    2018年9月23日 02:09:44

--- weoutput_machine.py
+++ weoutput_machine.py
@@ -2,14 +2,20 @@
 #用于输出数据之用
 #读取Dataframe，并且输出为需要的格式
 ###
+from sqlalchemy import create_engine
+import pandas as pd
 
 class Output:
-    def __init__(self,frame):
+    def __init__(self,frame,way):
         self.frame = frame
+        self.way = way
+
+    def sql_output(self,database_name,schema = 'hunbo',if_exist = 'replace'):
+        connect = create_engine('mysql+pymysql://root:!QAZ2wsx@localhost:3306/hunbo?charset=utf8')
+        try:
+            pd.io.sql.to_sql(self.frame,database_name, connect, schema=schema, if_exists=if_exist)
+            print('数据库生成成功')
+        except:
+            print('数据库生成失败')
 
 
-    def sql_output(self):
-
-    def file_output(self):
-
-    def judge_outputway(self):
