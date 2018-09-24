bfcf7346e5b910451a8b34987185d5d59fa752ad
ljc9562 <854426089@qq.com>
2018-09-24 20:58:51 +0800

    2018年9月23日 02:09:44

--- Lable_machine_engine.py
+++ Lable_machine_engine.py
@@ -1,4 +1,2 @@
-import pandas as pd
 
-data = pd.read_hdf(r"Z:\automation\new_tot_table\output\tot_data.h5")
 
---
+++ wetagging_machine.py
@@ -0,0 +1,25 @@
+#tagging流程设计
+#1 正则模式  （动工）
+#2 脚本逻辑模式 （动工）
+#3 excel模块控制模式  （暂停，未了解接口情况）
+
+import pandas as pd
+
+class wetagging:
+    def __init__(self,df):
+        self.data_soure = df
+
+    def model_list(self):
+        pass
+
+    def re_model(self):
+
+
+
+    def script_model(self):
+
+
+    def tag_output(self):
+
+
+
